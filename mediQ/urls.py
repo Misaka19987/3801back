e6cda412236d48f067104768118e00cdc39d97d3
Misaka19987 <jiaan.wang1@uqconnect.edu.au>
2021-10-19 03:31:06 +0800

    V0.3
    Hyperlink problem has been fixed;
    sign-up page is now added

--- mediQ/urls.py
+++ mediQ/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from MediQ.views import *
 
 urlpatterns = [
-    path('', index),
-    path('symptom-checker.html/', checker),
-    path('medical-service.html/', service),
-    path('online-booking.html/', booking),
-    path('medical-insurance.html/', insur)
+    path('', index, name='index'),
+    path('symptom-checker.html/', checker, name='checker'),
+    path('medical-service.html/', service, name='service'),
+    path('online-booking.html/', booking, name='booking'),
+    path('medical-insurance.html/', insur, name='insurance'),
+    path('sign-up.html', signup, name='signup'),
+    path('reset-password.html/', reset, name='reset')
 ]
+app_name = 'MediQ'
